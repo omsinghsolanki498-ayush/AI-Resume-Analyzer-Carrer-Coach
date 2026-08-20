78ba87bb751482f8f3d3df5474589a821e896567
omsinghsolanki498@gmail.com <omsinghsolanki498@gmail.com>
2026-08-20 13:50:30 +0530

    update project

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
+import App from "./App.jsx";
 import "./index.css";
 
-import { DashboardProvider } from "./context/DashboardContext";
+import { DashboardProvider } from "./context/DashboardContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
