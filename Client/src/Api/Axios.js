acef10e2c0911bb139d99abefd0fd30409593d87
omsinghsolanki498@gmail.com <omsinghsolanki498@gmail.com>
2026-08-20 12:35:12 +0530

    fix vite dependency and api url

--- Client/src/Api/Axios.js
+++ Client/src/Api/Axios.js
@@ -1,24 +1,18 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    "https://ai-resume-analyzer-carrer-coach.onrender.com/api",
-
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-    if (token) {
-      config.headers.Authorization =
-        `Bearer ${token}`;
-    }
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+  return config;
+});
 
 export default api;
